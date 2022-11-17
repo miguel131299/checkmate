155731da112e5a6d1ef72cf009992dfc2f63a029
miguel131299 <miguel131299@gmail.com>
2022-11-17 16:32:29 +0100

    Reset Slider after every Question

--- src/components/DiscreteSliderMarks.jsx
+++ src/components/DiscreteSliderMarks.jsx
@@ -1,5 +1,7 @@
 import Box from "@mui/material/Box";
 import Slider from "@mui/material/Slider";
+import { Context } from "../Context";
+import { useContext } from "react";
 
 const marks = [
   {
@@ -32,9 +34,14 @@ function getWidth() {
 getWidth();
 
 export default function DiscreteSliderMarks(props) {
+  // use to reset value of slider
+
+  const { currentQuestion } = useContext(Context);
+
   return (
     <Box sx={{ width: getWidth() > 992 ? 600 : 300 }}>
       <Slider
+        key={currentQuestion.id}
         sx={{ color: "#90dea4" }}
         defaultValue={2}
         marks={marks}
